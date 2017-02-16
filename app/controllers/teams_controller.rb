c7c87a72f03922a4729a047c7205063dd10f752d
rogeruwayezu <rogeruwayezu@gmail.com>
2017-02-16 14:01:13 +0200

    added index and show action

---
+++ app/controllers/teams_controller.rb
@@ -0,0 +1,25 @@
+class TeamsController < ApplicationController
+  def index
+    @teams = Unirest.get("http://localhost:3000/api/v2/teams.json").body
+  end
+  def show
+    @team = Unirest.get("http://localhost:3000/api/v2/teams/#{params[:id]}.json").body
+  end
+  def new
+
+  end
+  def create
+    @team = Unirest.post("http://localhost:3000/api/v2/teams.json", :headers => {"Accept"=> "application/json"}, :parameters => {:creature => params[:creature], :name => [:name], :state => params[:state], :sport => params[:sport]}
+      redirect_to "/teams/#{@team['id']}"
+
+  end
+  def edit
+
+  end
+  def update
+
+  end
+  def destroy
+
+  end
+end
