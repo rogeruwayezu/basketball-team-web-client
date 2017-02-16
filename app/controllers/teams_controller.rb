16c98c103aa9b04f323594c30dcea926331df305
rogeruwayezu <rogeruwayezu@gmail.com>
2017-02-16 20:19:32 +0200

    complete Restful action

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -6,7 +6,7 @@ class TeamsController < ApplicationController
     @team = Unirest.get("http://localhost:3000/api/v2/teams/#{params[:id]}.json").body
   end
   def new
-
+ 
   end
   def create
     @team = Unirest.post("http://localhost:3000/api/v2/teams.json", :headers => {"Accept"=> "application/json"}, :parameters => {:creature => params[:creature], :name => params[:name], :state => params[:state], :sport => params[:sport]}).body
@@ -14,12 +14,16 @@ class TeamsController < ApplicationController
 
   end
   def edit
-
+    @team = Unirest.get("http://localhost:3000/api/v2/teams/#{params[:id]}.json").body
   end
   def update
+     @team = Unirest.patch("http://localhost:3000/api/v2/teams/#{params[:id]}.json", :headers => {"Accept"=> "application/json"}, :parameters => {:creature => params[:creature], :name => params[:name], :state => params[:state], :sport => params[:sport]}).body
+
+     redirect_to "/teams/#{@team['id']}"
 
   end
   def destroy
-
+    @team = Unirest.delete("http://localhost:3000/api/v2/teams/#{params[:id]}.json").body
+    redirect_to "/teams"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "/teams", to: 'teams#index'
-  get "/teams", to: 'teams#new'
+  get "/teams/new", to: 'teams#new'
   post"/teams", to: 'teams#create'
   get "/teams/:id", to:'teams#show'
-  get "/teams/edit/:id", to: 'teams#edit'
+  get "/teams/:id/edit", to: 'teams#edit'
   patch "/teams/:id", to:'teams#update'
   delete "/teams/:id", to:'teams#destroy'
 end
