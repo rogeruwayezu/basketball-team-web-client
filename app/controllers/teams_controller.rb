ff34332f14027696b10332417a21ba5b35a2162a
rogeruwayezu <rogeruwayezu@gmail.com>
2017-02-20 10:26:26 +0200

    added destroy action

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,16 +1,15 @@
 class TeamsController < ApplicationController
   def index
-    @teams = Unirest.get("http://localhost:3000/api/v2/teams.json").body
+   @teams = Team.all
   end
   def show
-    team_hash = Unirest.get("http://localhost:3000/api/v2/teams/#{params[:id]}.json").body
-    @team = Team.new(team_hash)
+    @team = Team.find(params[:id])
   end
   def new
  
   end
   def create
-    @team = Unirest.post("http://localhost:3000/api/v2/teams.json", :headers => {"Accept"=> "application/json"}, :parameters => {:creature => params[:creature], :name => params[:name], :state => params[:state], :sport => params[:sport]}).body
+      
       redirect_to "/teams/#{@team['id']}"
 
   end
@@ -24,7 +23,8 @@ class TeamsController < ApplicationController
 
   end
   def destroy
-    @team = Unirest.delete("http://localhost:3000/api/v2/teams/#{params[:id]}.json").body
+    @team = Team.find(params[:id])
+    @team.destroy
     redirect_to "/teams"
   end
 end
--- app/models/concerns/team.rb
+++ app/models/concerns/team.rb
@@ -8,5 +8,23 @@ class Team
     @state = hash["state"]
 
   end
-
+  def self.find(id)
+  team_hash = Unirest.get("http://localhost:3000/api/v2/teams/#{id}.json").body
+  @team = Team.new(team_hash)
+  end
+  def self.all
+    team_hashes = Unirest.get("http://localhost:3000/api/v2/teams.json").body
+    @teams = []
+    team_hashes.each do |employee_hash|
+      @teams << Team.new(employee_hash)
+    end
+    @teams
+  end
+  def destroy
+    team_hash = Unirest.delete("http://localhost:3000/api/v2/teams/#{id}.json").body
+  end
+  def self.create
+    team_hash = Unirest.post("http://localhost:3000/api/v2/teams.json", :headers => {"Accept"=> "application/json"}, :parameters => {:creature => params[:creature], :name => params[:name], :state => params[:state], :sport => params[:sport]}).body
+     @team = Team.new(@team_hash)
+  end
 end
